f52b8a2bb0adb24d52aaee7a570344f924a8e854
Flavien LEMAISTRE <flavien.lemaistre@free.fr>
2024-09-02 12:28:38 +0200

    Embellir le code

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -1,60 +1,67 @@
-window.addEventListener('DOMContentLoaded',()=>{
-  const pokeP=document.getElementById('pokeInfo')
-  const pokeAbility=document.getElementById('pokeAbility')
-  const pokeDiv=document.getElementById('pokemon-info')
-  const pokeAbilityBtn=document.getElementById('ability')
-  const sendButton=document.getElementById('sendButton')
-  
+window.addEventListener("DOMContentLoaded", () => {
+  const pokeP = document.getElementById("pokeInfo");
+  const pokeAbility = document.getElementById("pokeAbility");
+  const pokeDiv = document.getElementById("pokemon-info");
+  const pokeAbilityBtn = document.getElementById("ability");
+  const sendButton = document.getElementById("sendButton");
+
   /**
    * Rends visible la div "comment" ainsi que le formulaire contenu dedans.
    * Met à jour le contenu de la section
    * @param {event} event L'évenement qui déclenche cette fonction.
    */
-  const displayComment=(event)=>{
-    const comment=document.getElementById("myComment")
-    event.preventDefault()
-    comment.style.visibility="visible"
-    $("#message").html($("#messageInput").val())
-  }
+  const displayComment = (event) => {
+    const comment = document.getElementById("myComment");
+    event.preventDefault();
+    comment.style.visibility = "visible";
+    $("#message").html($("#messageInput").val());
+  };
 
   /**
    * Fonction qui va chercher aléatoirement un pokémon parmi 896 disponible
    * dans une api.
    * "foundPokemon" stock la réponse en entier.
-   * Si "foundPokemon" est valide, "jsonPokemon" stock le json parser de 
-   * "foundPokemon". L'ojet "pokeInfo" va alors servir a stocker l'espèce de 
+   * Si "foundPokemon" est valide, "jsonPokemon" stock le json parser de
+   * "foundPokemon". L'ojet "pokeInfo" va alors servir a stocker l'espèce de
    * pokémon après l'avoir formaté.
-   * Une fois fait, les informations seront visible dans une div qui sera 
+   * Une fois fait, les informations seront visible dans une div qui sera
    * préalablement remise à zero si elle contenait déjà quelque chose
-   * Des messages d'erreur seront affichés dans la console error. 
-   * Si aucun pokémon n'est trouvé mais que tout c'est bien passé, alors un 
+   * Des messages d'erreur seront affichés dans la console error.
+   * Si aucun pokémon n'est trouvé mais que tout c'est bien passé, alors un
    * message indiquera qu'aucun pokémon n'a été trouvé
    */
-  const fetchPokemon=async ()=>{
-    const pokedexNum=Math.floor(Math.random() * 897)
-    let foundPokemon=''
-    let jsonPokemon=''
-    const pokeInfo={}
+  const fetchPokemon = async () => {
+    const pokedexNum = Math.floor(Math.random() * 897);
+    let foundPokemon = "";
+    let jsonPokemon = "";
+    const pokeInfo = {};
 
-    try{
-      foundPokemon=await fetch(`https://pokeapi.co/api/v2/pokemon/${pokedexNum}`, {method: 'GET', headers: {'Content-Type': 'application/json'}})
+    try {
+      foundPokemon = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokedexNum}`,
+        { method: "GET", headers: { "Content-Type": "application/json" } },
+      );
+    } catch (error) {
+      console.error(error.message);
     }
-    catch(error){console.error(error.message)}
 
-    if(foundPokemon){
-      try{
-        jsonPokemon=await foundPokemon.json()
-        pokeInfo.name=`${String(jsonPokemon.species.name).slice(0, 1).toUpperCase()}${String(jsonPokemon.species.name).slice(1, jsonPokemon.species.name.length).toLowerCase()}`
+    if (foundPokemon) {
+      try {
+        jsonPokemon = await foundPokemon.json();
+        pokeInfo.name = `${String(jsonPokemon.species.name).slice(0, 1).toUpperCase()}${String(jsonPokemon.species.name).slice(1, jsonPokemon.species.name.length).toLowerCase()}`;
+      } catch (error) {
+        console.error(error.message);
       }
-      catch(error){console.error(error.message)}
-
+    } else {
+      jsonPokemon = "No Pokémon found...";
     }
-    else{jsonPokemon='No Pokémon found...'}
 
-    if(pokeP.innerText !== ''){pokeP.innerText=''}
-    pokeP.innerText=`Your Pokémon is ${pokeInfo.name}.`
-    pokeAbilityBtn.removeAttribute('disabled')
-  }
+    if (pokeP.innerText !== "") {
+      pokeP.innerText = "";
+    }
+    pokeP.innerText = `Your Pokémon is ${pokeInfo.name}.`;
+    pokeAbilityBtn.removeAttribute("disabled");
+  };
 
   /**
    * Fonction qui attribu au Pokemon de l'utilisateur un pouvoir aléatoire parmi
@@ -65,63 +72,71 @@ window.addEventListener('DOMContentLoaded',()=>{
    * Si foundAbilities n'est pas valide (après le fetch), alors la variable sera
    * rempli avec un message d'erreur.
    */
-  const fetchPokemonAbilities=async()=>{
-    const pokedexNum=Math.floor(Math.random() * 266)
-    let foundAbilities=''
-    const pokeAbility=document.getElementById('pokeAbility')
-    let jsonAbilities={}
-    let abilityToDisplay=''
-  
+  const fetchPokemonAbilities = async () => {
+    const pokedexNum = Math.floor(Math.random() * 266);
+    let foundAbilities = "";
+    const pokeAbility = document.getElementById("pokeAbility");
+    let jsonAbilities = {};
+    let abilityToDisplay = "";
+
     try {
-      foundAbilities=await fetch(`https://pokeapi.co/api/v2/ability/${pokedexNum}`, {method: 'GET', headers: {'Content-Type': 'application/json'}})
+      foundAbilities = await fetch(
+        `https://pokeapi.co/api/v2/ability/${pokedexNum}`,
+        { method: "GET", headers: { "Content-Type": "application/json" } },
+      );
+    } catch (error) {
+      console.error(error.message);
     }
-    catch(error){console.error(error.message)}
 
-    if(foundAbilities){
-      try{
-        jsonAbilities=await foundAbilities.json();
-        if('' !== jsonAbilities.name && undefined !== jsonAbilities.name){
-          abilityToDisplay=`${String(jsonAbilities.name).slice(0, 1).toUpperCase()}${String(jsonAbilities.name).slice(1, jsonAbilities.name.length).toLowerCase()}`
+    if (foundAbilities) {
+      try {
+        jsonAbilities = await foundAbilities.json();
+        if ("" !== jsonAbilities.name && undefined !== jsonAbilities.name) {
+          abilityToDisplay = `${String(jsonAbilities.name).slice(0, 1).toUpperCase()}${String(jsonAbilities.name).slice(1, jsonAbilities.name.length).toLowerCase()}`;
+        } else {
+          abilityToDisplay = "Tackle";
         }
-        else{abilityToDisplay='Tackle'}
+      } catch (error) {
+        console.error(error.message);
       }
-      catch(error){console.error(error.message)}
-
+    } else {
+      jsonAbilities = "No ability found...";
     }
-    else{jsonAbilities='No ability found...'}
 
-    if(pokeAbility.innerText !== ''){pokeAbility.innerText=''}
+    if (pokeAbility.innerText !== "") {
+      pokeAbility.innerText = "";
+    }
 
-    pokeAbility.innerText=`It now knows the move ${abilityToDisplay}!`
-  }
-  
+    pokeAbility.innerText = `It now knows the move ${abilityToDisplay}!`;
+  };
+
   /**
-   * Ecoute l'evenement click sur un bouton pour déclencher la fonction 
+   * Ecoute l'evenement click sur un bouton pour déclencher la fonction
    * fetchPokemon
    * Puis ajoute une div pour afficher le résultat.
    */
-  const invoquePokemon=()=>{
-    const pokeBtn=document.getElementById('pokemon')
-    pokeBtn.addEventListener('click', fetchPokemon)
-    pokeDiv.appendChild(pokeP)
-  }
+  const invoquePokemon = () => {
+    const pokeBtn = document.getElementById("pokemon");
+    pokeBtn.addEventListener("click", fetchPokemon);
+    pokeDiv.appendChild(pokeP);
+  };
 
   /**
    * Ecoute l'evenement click sur un bouton pour déclencher la fonction
    * fetchPokemonAbilities.
    * Puis ajoute une div pour afficher le résultat.
    */
-  const pokemonAbility=()=>{
-    pokeAbilityBtn.addEventListener('click', fetchPokemonAbilities)
+  const pokemonAbility = () => {
+    pokeAbilityBtn.addEventListener("click", fetchPokemonAbilities);
     pokeDiv.appendChild(pokeAbility);
-  }
-  
-  /**  
+  };
+
+  /**
    * Fonction IIFE qui lance les autres fonction de ce fichier.
    */
-  (function startAll(){
-    invoquePokemon()
-    pokemonAbility()
-    sendButton.addEventListener('click', displayComment)
-  })()
-})
+  (function startAll() {
+    invoquePokemon();
+    pokemonAbility();
+    sendButton.addEventListener("click", displayComment);
+  })();
+});
